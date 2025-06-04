8479304408a46069a662321120164d97d4eb5652
BantAlex <bantalex3@gmail.com>
2025-06-04 19:20:21 +0300

    Refactor a bit of #generate_move. It's still ass

--- lib/knight_travails.rb
+++ lib/knight_travails.rb
@@ -7,48 +7,46 @@ class KnightTravails
   def initialize(start,fin)
     @chess = Board.new
     knight_moves(start,fin)
-    print "You're already here!" if start == fin
+    puts "You're already here!" if start == fin
   end
 
-  def generate_move(start,fin) #Maybe move "start" to init
+  def generate_move(start) #Maybe move "start" to init
     @chess.board[start[0]][start[1]] = "K" #Place Knight On board
     bounds = (0..7)
     abort ("Invalid Option") unless start.all?{ |n| bounds.include?(n)}
 
     #TODO - Refactor this code. This is ass. It works...but its ass
-    moveset = [
+    moveset = [ #Calculated based on knights moveset on the created board
       (start[0] - 2), (start[0] - 1),
       (start[0] + 2), (start[0] + 1),
       (start[1] - 2), (start[1] - 1),
       (start[1] + 2), (start[1] + 1)
     ]
 
-    #Find possible moves
-    @chess.board[moveset[0]][moveset[5]] = "M" if bounds.include?(moveset[0]) &&  bounds.include?(moveset[5])
-    @chess.board[moveset[0]][moveset[7]] = "M" if bounds.include?(moveset[0]) &&  bounds.include?(moveset[7])
+    move_cordinates = [ #Possible moves the knight can make
+      [moveset[0],moveset[5]],[moveset[0],moveset[7]],
+      [moveset[1],moveset[4]],[moveset[1],moveset[6]],
+      [moveset[3],moveset[4]],[moveset[3],moveset[6]],
+      [moveset[2],moveset[5]],[moveset[2],moveset[7]],
+    ]
 
-    @chess.board[moveset[1]][moveset[4]] = "M" if bounds.include?(moveset[1]) &&  bounds.include?(moveset[4])
-    @chess.board[moveset[1]][moveset[6]] = "M" if bounds.include?(moveset[1]) &&  bounds.include?(moveset[6])
-
-    @chess.board[moveset[3]][moveset[4]] = "M" if bounds.include?(moveset[3]) &&  bounds.include?(moveset[4])
-    @chess.board[moveset[3]][moveset[6]] = "M" if bounds.include?(moveset[3]) &&  bounds.include?(moveset[6])
-
-    @chess.board[moveset[2]][moveset[5]] = "M" if bounds.include?(moveset[2]) &&  bounds.include?(moveset[5])
-    @chess.board[moveset[2]][moveset[7]] = "M" if bounds.include?(moveset[2]) &&  bounds.include?(moveset[7])
-    #?Maybe re-init when checking from next move
+    move_cordinates.each do |r,c|
+      @chess.board[r][c] = "M" if bounds.include?(r) &&  bounds.include?(c)
+    end
   end
 
   def knight_moves(start,fin) #while start != fin
-    generate_move(start,fin)
+    generate_move(start) #Place knight on board find avaiable moves
     #TODO - Somehow make start = the move that was made
-    #TODO - Itereate (recursivly?) over all the possible moves and keep count, returing the lease resistance
+    #TODO - First iterate through the actual board and find all the "M"s?
+      #*For each "M" push the cordinate into an array?
+    #TODO - Then itereate (recursivly?) over all the possible moves and keep count, returing the lease resistance
     @chess.print_board
   end
 end
 
-play = KnightTravails.new([0,0],[0,0]) #[Letter,Number] - 0 to 7
+play = KnightTravails.new([3,4],[0,0]) #[Letter,Number] - 0 to 7
 
-#Connections from D3: C1,E1,B2,F2,B4,F4,C5,E5
-# knight_moves([A,0],[B,2]) Should return the shortest path that the knight can take to reach start A0 to B2:A0->B2
+# knight_moves([A,0],[B,2]) Should return the shortest path that the knight can take to go from A0 to B2
 # knight_moves([A,0],[D,3]) Should return: A0->C1->D3
 #[2,4]+[1,5] + K + [1,5]+[2,4]
